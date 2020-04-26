c424d3dffe7b260a72841af331c4b02035e042d8
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2020-04-26 21:56:55 +0200

    Updated selectors

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ function twoDigitFormat(number) {
     }
 
     //wait for website to load data
-    const tableFundPricesSelector = "[au-target-id='1021']";
-    const tableFundNamesSelector = "[au-target-id='1000']";
+    const tableFundPricesSelector = "[au-target-id='1014']";
+    const tableFundNamesSelector = "[au-target-id='993']";
 
     const tableSelectors = [tableFundPricesSelector,tableFundNamesSelector];
     for(const tableSelector of tableSelectors) {
